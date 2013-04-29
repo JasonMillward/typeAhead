dfb1b6eba2ede6426ca260477921f370fe4f0847
Jason Millward <jason@jcode.me>
2013-04-29 13:17:23 +0930

    cleaned up directory structure

---
+++ js/typeAhead.js
@@ -0,0 +1,37 @@
+$(function () {
+    $('#myTable').find('tbody').empty();
+    $('#list').empty();
+    $("#brands").typeahead({
+        source: function (query, process) {
+            return $.get('./php/data.php', {
+                query: query
+            }, function (data) {
+                return process(data);
+            }, "json");
+        },
+        updater: function (item) {
+            var isUnique = true;
+            $('#myTable').find('tbody').find('tr').each(function () {
+                var text = $(this).find('td').text().replace('×', '');
+                if (item == text) {
+                    isUnique = false;
+                }
+            });
+            if (isUnique) {
+                $('#myTable').find('tbody').append($('<tr>').append($('<td>').text(item)).append($('<td>').append($('<a>').addClass("close").attr("href", "#").html("&times;"))));
+            }
+        }
+    });
+    $('#submitButton').click(function () {
+        $('#output').show();
+        $('#list').empty();
+        $('#myTable').find('tbody').find('tr').each(function () {
+            var text = $(this).find('td').text().replace('×', '');
+            $('#list').append(text).append("<br/>");
+        });
+        $('#myTable').find('tbody').empty();
+    });
+    $(document).on("click", ".close", function () {
+        $(this).parent().parent().remove();
+    });
+});
